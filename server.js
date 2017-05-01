98e9f27b736005880827ed4165465a09ce9f841e
Aditi Sharma <aditisharmabtp@gmail.com>
2017-05-01 23:03:22 +0530

    changes for project page

--- server.js
+++ server.js
@@ -37,6 +37,12 @@ app.get('/', (request, response) => {
     });
 });
 
+app.get('/projects', (request, response) => {
+    response.render('projects.hbs', {
+         pageTitle: 'Projects'
+    });
+});
+
 app.get('/about', (request, response) => {
     response.render('about.hbs', {
         pageTitle: 'About Page'
